d0d32237d67973fd8d495ae0e25ec7af13e109b5
Josh Hayes <jhayes0128@gmail.com>
2018-04-06 00:18:50 -0500

    added Flask/Browser capabilites

---
+++ old_trade.py
@@ -0,0 +1,19 @@
+import sys
+import time
+from orders import buy_market, buy_limit, sell_limit, sell_market, order_status
+
+
+
+#Hardcode trading pair
+product_id = 'BTC-USD'
+
+while True:
+    buy = buy_market("BTC-USD", 0.01)
+    buy_order_id = buy["id"]
+    buy_info = order_status(buy_order_id)
+    buy_price = float(buy_info["executed_value"])
+    buy_amount = buy_info["size"]
+
+    print(time.strftime("%a, %d %b %Y %H:%M:%S"), "price:" + str(buy_price * 100), "amount:" + buy_amount)
+
+    time.sleep(5)
--- trade.py
+++ trade.py
@@ -2,20 +2,52 @@ import sys
 import time
 from orders import buy_market, buy_limit, sell_limit, sell_market, order_status
 from flask import Flask, request, redirect, render_template
-from flask_sqlalchemy import flask_sqlalchemy
 
-
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
 #Hardcode trading pair
 product_id = 'BTC-USD'
 
-while True:
+last_trade_time = ""
+last_trade_price = ""
+last_trade_amount = ""
+
+x = "testing"
+
+def trade():
     buy = buy_market("BTC-USD", 0.01)
     buy_order_id = buy["id"]
+    """
     buy_info = order_status(buy_order_id)
     buy_price = float(buy_info["executed_value"])
-    buy_amount = buy_info["size"]
 
-    print(time.strftime("%a, %d %b %Y %H:%M:%S"), "price:" + str(buy_price * 100), "amount:" + buy_amount)
+    trade_time = time.strftime("%a, %d %b %Y %H:%M:%S")
+    trade_price = str(buy_price * 100)
+    trade_amount = buy_info["size"]
 
-    time.sleep(5)
+    last_trade_time = trade_time
+    last_trade_price = trade_price
+    last_trade_amount = str(trade_amount) """
+    return buy_order_id
+    
+    #print(trade_time, trade_price, trade_amount)
+
+    #time.sleep(5)
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    trade_id = trade()
+    buy_info = order_status(trade_id)
+    buy_price = float(buy_info["executed_value"])
+
+    trade_time = time.strftime("%a, %d %b %Y %H:%M:%S")
+    trade_price = str(buy_price * 100)
+    trade_amount = buy_info["size"]
+    
+    return render_template("index.html", time=trade_time, price=trade_price, amount=trade_amount)
+    #return render_template("index.html", time=last_trade_time, price=last_trade_price, amount=last_trade_amount)
+
+
+app.run()
